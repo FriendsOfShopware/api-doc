de1ea4a59057c53b33e660fcb1ad6efa2163b4bd
Shyim <6224096+shyim@users.noreply.github.com>
2020-09-08 10:19:32 +0000

    Add dumper

---
+++ dumper.php
@@ -0,0 +1,77 @@
+<?php
+
+$url = $_SERVER['argv'][1];
+
+function fetch($path) {
+    global $url;
+
+    $ch = curl_init($url . $path);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $json = curl_exec($ch);
+    curl_close($ch);
+
+    if (empty($json)) {
+        return null;
+    }
+
+    $api = json_decode($json, true);
+    $api['servers'] = [
+        [
+            'url' => 'http://localhost'
+        ]
+    ];
+
+    return $api;
+}
+
+function update(string $currentVersion)
+{
+    $updateInfo = json_decode(file_get_contents('https://update-api.shopware.com/v1/release/update?shopware_version=' . $currentVersion . '&channel=stable&major=6'), true);
+
+    if ($updateInfo['version'] === $currentVersion) {
+        return;
+    }
+
+    $zipName = basename($updateInfo['uri']);
+
+    echo '=> Downloading ' . $updateInfo['version'] . PHP_EOL;
+    exec('wget ' . $updateInfo['uri']);
+
+    echo '=> Unzip ' . $updateInfo['version'] . PHP_EOL;
+    exec('unzip -o ' . $zipName);
+
+    echo '=> Updating ' . $updateInfo['version'] . PHP_EOL;
+    exec('public/recovery/update/index.php -n');
+    exec('rm -rf update-assets');
+
+    dump($updateInfo['version']);
+}
+
+function dump(string $currentVersion) {
+    $apiVersion = substr($currentVersion, 2, 1);
+
+    $api = fetch('/api/v' . $apiVersion . '/_info/openapi3.json');
+    $scApi = fetch('/sales-channel-api/v' . $apiVersion . '/_info/openapi3.json');
+    $stApi = fetch('/store-api/v' . $apiVersion . '/_info/openapi3.json');
+
+    $outputDir = dirname(__DIR__) . '/api-doc/version/' . $currentVersion . '/';
+    if (!file_exists($outputDir)) {
+        mkdir($outputDir, 0777, true);
+    }
+
+    if ($api) {
+        file_put_contents($outputDir . '/api.json', json_encode($api));
+    }
+
+    if ($scApi) {
+        file_put_contents($outputDir . '/sales-channel-api.json', json_encode($scApi));
+    }
+
+    if ($stApi) {
+        file_put_contents($outputDir . '/store-api.json', json_encode($stApi));
+    }
+
+    update($currentVersion);
+}
+
+dump('6.1.0');
