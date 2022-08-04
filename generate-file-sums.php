15f7474f3e778d500a0ae1eecd1ff3f399d33b92
tinect <s.koenig@tinect.de>
2022-08-04 12:51:56 +0200

    feat: ignore some specific files for file-sums

--- generate-file-sums.php
+++ generate-file-sums.php
@@ -1,6 +1,6 @@
 <?php
 
-$forceGenerate = isset($_SERVER['FORCE_GENERATE']) ? $_SERVER['FORCE_GENERATE'] === '1' : false;
+$forceGenerate = isset($_SERVER['FORCE_GENERATE']) && $_SERVER['FORCE_GENERATE'] === '1';
 
 echo "Force generate mode: " . var_export($forceGenerate);
 
@@ -11,15 +11,15 @@ function getMissingVersions(array $releases): array
     $missing = [];
 
     foreach ($releases as $release) {
-        $filename = dirname(__DIR__) . '/api-doc/version/' . $release['version'] . '/Files.md5sums';
-        if (is_file($filename) && !$forceGenerate) {
+        $folder = dirname(__DIR__) . '/api-doc/version/' . $release['version'];
+
+        $filename = $folder . '/Files.md5sums';
+        if (!$forceGenerate && is_file($filename)) {
             continue;
         }
 
-        $folder = dirname(__DIR__) . '/api-doc/version/' . $release['version'];
-
-        if (!file_exists(dirname(__DIR__) . '/api-doc/version/' . $release['version'])) {
-            if (!mkdir($concurrentDirectory = dirname(__DIR__) . '/api-doc/version/' . $release['version']) && !is_dir($concurrentDirectory)) {
+        if (!file_exists($folder)) {
+            if (!mkdir($concurrentDirectory = $folder) && !is_dir($concurrentDirectory)) {
                 throw new \RuntimeException(sprintf('Directory "%s" was not created', $concurrentDirectory));
             }
         }
@@ -32,6 +32,33 @@ function getMissingVersions(array $releases): array
 
 $releases = array_reverse(json_decode(file_get_contents('https://update-api.shopware.com/v1/releases/install?major=6'), true));
 $missingVersions = getMissingVersions($releases);
+
+$ignoredFiles = [
+    'vendor/shopware/recovery/Common/vendor/autoload.php',
+    'vendor/shopware/recovery/Common/vendor/composer/ClassLoader.php',
+    'vendor/shopware/recovery/Common/vendor/composer/InstalledVersions.php',
+    'vendor/shopware/recovery/Common/vendor/composer/autoload_classmap.php',
+    'vendor/shopware/recovery/Common/vendor/composer/autoload_files.php',
+    'vendor/shopware/recovery/Common/vendor/composer/autoload_namespaces.php',
+    'vendor/shopware/recovery/Common/vendor/composer/autoload_psr4.php',
+    'vendor/shopware/recovery/Common/vendor/composer/autoload_real.php',
+    'vendor/shopware/recovery/Common/vendor/composer/autoload_static.php',
+    'vendor/shopware/recovery/Common/vendor/composer/installed.php',
+    'vendor/shopware/recovery/vendor/autoload.php',
+    'vendor/shopware/recovery/vendor/composer/ClassLoader.php',
+    'vendor/shopware/recovery/vendor/composer/InstalledVersions.php',
+    'vendor/shopware/recovery/vendor/composer/autoload_classmap.php',
+    'vendor/shopware/recovery/vendor/composer/autoload_namespaces.php',
+    'vendor/shopware/recovery/vendor/composer/autoload_psr4.php',
+    'vendor/shopware/recovery/vendor/composer/autoload_real.php',
+    'vendor/shopware/recovery/vendor/composer/autoload_static.php',
+];
+
+$ignoredFilesFilter = [];
+
+foreach ($ignoredFiles as $ignoredFile) {
+    $ignoredFilesFilter[] = '-not -iwholename \'' . $ignoredFile . '\'';
+}
 
 foreach ($missingVersions as $release) {
     $installFolder = sys_get_temp_dir() . '/' . uniqid('sw', true);
@@ -46,7 +73,7 @@ foreach ($missingVersions as $release) {
     exec('wget -O install.zip -qq ' . $release['uri']);
     exec('unzip -q install.zip');
 
-    exec("find vendor/shopware -type f \( -iname '*.php' -o -iname '*.twig' -o -iname '*.js' -o -iname '*.scss' -o -iname '*.xml' \) -print0 | xargs -0 md5sum | sort -k 2 -d > " . $release['output']);
+    exec("find vendor/shopware -type f \( " . implode(' ', $ignoredFilesFilter) . " -iname '*.php' -o -iname '*.twig' -o -iname '*.js' -o -iname '*.scss' -o -iname '*.xml' \) -print0 | xargs -0 md5sum | sort -k 2 -d > " . $release['output']);
 
     exec('rm -rf ' . escapeshellarg($installFolder));
 }
